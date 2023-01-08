9221f844f2a090935afb4043b10d9784aa3ff278
Iskander <kubeev2001@gmail.com>
2023-01-08 17:10:04 +0600

    Hero added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import TheHeader from "./components/TheHeader.vue"
+import TheHero from "./components/TheHero.vue"
 </script>
 
 <template>
   <TheHeader/>
+  <TheHero/>
   <RouterView />
 </template>
---
+++ src/components/TheHero.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="hero section">
+    <div class="container">
+      <div class="hero__wrapper">
+        <img src="../assets/images/image.jpg" alt="" class="hero__img">
+        <div class="hero__text">
+          <h1 class="hero__text-title">Take your podcast to the <span class="underline">next</span> <span class="bold">level</span></h1>
+
+          <div class="hero__links">
+            <p class="hero__links">Listen on</p>
+          <div class="hero__wrapper-list">
+            <a href="https://spotify.com" target="_blank" class="hero__wrapper-link"><img src="../assets/images/icons/spotify.svg" alt="spotify"></a>
+            <a href="https://pocketcasts.com" target="_blank" class="hero__wrapper-link"><img src="../assets/images/icons/pocket_casts.svg" alt="pocket casts"></a>
+            <a href="https://soundcloud.com" target="_blank" class="hero__wrapper-link"><img src="../assets/images/icons/soundcloud.svg" alt="soundcloud"></a>
+            <a href="https://podcasts.apple.com" target="_blank" class="hero__wrapper-link"><img src="../assets/images/icons/apple_podcasts.svg" alt="apple podcasts"></a>
+            <a href="https://overcast.fm" target="_blank" class="hero__wrapper-link"><img src="../assets/images/icons/overcast.svg" alt="overcast"></a>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+  </section>
+</template>
+
+<script>
+
+</script>
