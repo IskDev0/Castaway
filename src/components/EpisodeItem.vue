7abe08dc673cbcdc64a6f1b80714e546fa40d118
Iskander <kubeev2001@gmail.com>
2023-01-09 19:02:53 +0600

    Added responsibility

--- src/components/EpisodeItem.vue
+++ src/components/EpisodeItem.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="episode">
-    <img :src="image" alt="podcast image">
+    <img class="episode__img" :src="image" alt="podcast image">
     <div class="episode__text">
       <p class="episode__genre">{{ episode.genre }}</p>
-      <p class="episode__number section__subtitle">Episode {{ episode.id }}</p>
-      <p class="episode__title section__title">{{ episode.title }}</p>
-      <p class="episode__desc desc">{{ episode.body }}</p>
+      <p class="section__subtitle episode__number">Episode {{ episode.id }}</p>
+      <p class="section__title episode__title">{{ episode.title }}</p>
+      <p class="desc episode__desc">{{ episode.body }}</p>
       <button @click="$router.push(`episodes/${episode.id}`)" class="episode__btn btn">View Episode Details</button>
     </div>
   </div>
